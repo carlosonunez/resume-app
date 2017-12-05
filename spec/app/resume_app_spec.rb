4c1b773827d19c3eee517224d68d8758a1ed7fc5
Carlos Nunez <dev@carlosnunez.me>
2017-12-04 18:12:57 -0600

    [spec/app/resume_app_spec.rb] dev@carlosnunez.me  |  Kramdown adds ids to every header, which is pretty convenient\!

--- spec/app/resume_app_spec.rb
+++ spec/app/resume_app_spec.rb
@@ -8,14 +8,14 @@ describe 'ResumeApp' do
 
   context 'Empty Markdown' do
     it "Renders a page with no content" do
-      expected_html = ''
+      expected_html = "\n"
       allow(ResumeApp::Downloaders)
         .to receive(:retrieve_latest_resume_from_s3)
         .and_return('')
       
       get '/'
 
-      expect(last_response.body).to eq('')
+      expect(last_response.body).to eq(expected_html)
       expect(last_response.status).to eq 200
     end
   end
@@ -33,13 +33,9 @@ It has words. Some of them are **bold,** and some of them are *emphasized.*
       MARKDOWN
 
       expected_html_content = <<-HTML
-<html>
-<body>
-<p><h1>This is a document.</h1></p>
-<p><h2>A document can have many sections!</h2></p>
-<p>It has words. Some of them are <strong>bold,</strong> and some of them are <em>emphasized.</em>
-</body>
-</html>
+<h1 id="this-is-a-document">This is a document.</h1>\n
+<h2 id="a-document-can-have-many-sections">A document can have many sections!</h2>\n
+<p>It has words. Some of them are <strong>bold,</strong> and some of them are <em>emphasized.</em></p>
       HTML
       allow(ResumeApp::Downloaders)
         .to receive(:retrieve_latest_resume_from_s3)
