3fce5de8bc6206d5ef77bfd35033af947f5ad6bf
Carlos Nunez <dev@carlosnunez.me>
2017-12-11 16:18:59 -0600

    [spec/unit/rake/aws_helper_spec.rb] dev@carlosnunez.me  |  Add a feature taht checks for env vars before generating our JSON.

--- spec/unit/rake/aws_helper_spec.rb
+++ spec/unit/rake/aws_helper_spec.rb
@@ -11,4 +11,16 @@ describe 'Given a Rake helper that interfaces with ECS' do
         .to raise_error(IOError, "ecs_task_template.json is missing. Please provide it.")
     end
   end
+  context "When we generate a task JSON and have missing environment variables" do
+    ['S3_BUCKET_NAME','RESUME_NAME'].each do |required_env_var|
+      it "It should fail when #{required_env_var} is missing." do
+        allow(ENV)
+          .to receive(:[])
+          .with("#{required_env_var}")
+          .and_return("")
+        expect {RakeHelpers::AWS::ECS.generate_task_json!}
+          .to raise_error(IOError, "#{required_env_var} is missing. Please provide it.")
+      end
+    end
+  end
 end
