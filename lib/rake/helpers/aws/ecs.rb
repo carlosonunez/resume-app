4b04c639c3d825fdcae50816d166c9319952509a
Carlos Nunez <dev@carlosnunez.me>
2017-12-11 15:45:56 -0600

    [lib/rake/helpers/aws/ecs.rb] dev@carlosnunez.me  |  Merge fix.

--- lib/rake/helpers/aws/ecs.rb
+++ lib/rake/helpers/aws/ecs.rb
@@ -1,21 +1,4 @@
 #!/usr/bin/env ruby
-<<<<<<< HEAD
-
-# Helpers used by Rake to do stuff, like deploy to ECS.
-module RakeHelpers
-  # Helpers for AWS.
-  module AWS
-    # Helpers for AWS ECS, Amazon's custom container orchestration platform.
-    module ECS
-      def self.generate_task_json!
-        raise IOError,"ecs_task_template.json is missing. Please provide it." \
-          unless File.exist? "ecs_task_template.json"
-      end
-    end
-  end
-end
-=======
-#!/usr/bin/env ruby
 
 # Helpers used by Rake to do stuff, like deploy to ECS.
 module RakeHelpers
