801a3549f7c5a42d45bc4e6dbebfe55736ba56d3
Carlos Nunez <dev@carlosnunez.me>
2017-12-14 12:58:15 -0600

    [Gemfile] dev@carlosnunez.me  |  Add `rspec-shell-expectations` so that we can mock shell commands.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,9 @@ require 'rack/test'
 require 'aws-sdk'
 require 'colorize'
 require 'dotenv'
+require 'rspec/shell/expectations'
+
 Dotenv.load('.env.test')
+RSpec.configure do |configuration|
+  configuration.include Rspec::Shell::Expectations
+end
