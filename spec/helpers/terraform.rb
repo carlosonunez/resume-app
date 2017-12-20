3c5c1a384044603530728e1c4b4aaf51bc68dd85
Carlos Nunez <dev@carlosnunez.me>
2017-12-20 07:59:55 -0600

    [spec/helpers/terraform.rb] dev@carlosnunez.me  |  We need to be able to easily address Terraform resource arguments that are maps. This commit does this.

--- spec/helpers/terraform.rb
+++ spec/helpers/terraform.rb
@@ -12,7 +12,9 @@ module RSpecHelpers
     # I'm not sure why this is triggering this cop. I'm guessing that
     # this has something to do with the RSpec verbs.
     # Therefore, I'm disabling it for now.
+    # NOTE: I'm fully aware that this is better suited as an RSpec plugin.
     # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/PerceivedComplexity
     def self.run_tests(resource_name:,
                        requirements_hash:)
       RSpec.describe 'Given a repository of Terraform configurations',
@@ -22,7 +24,49 @@ module RSpecHelpers
             expect(@terraform_plan[resource_name]).not_to be nil
           end
           requirements_hash.each_key do |requirement|
-            test_name = requirements_hash[requirement][:test_name]
+            # Since RSpec doesn't easily support calling functions within
+            # modules, instead of building an array of requirements and
+            # recursively running tests on each one, we'll need to have
+            # users address the Terraform resource argument they want
+            # to test with dot-notation.
+            #
+            # Example: Given the following resource:
+            #
+            # "test_resource"."my_resource" {
+            #   foo = "bar"
+            #   baz {
+            #     quux = "cool"
+            #   }
+            # }
+            #
+            # If I wanted to ensure that 'quux' is set to "cool", I'd need to
+            # do this:
+            #
+            # requirements = {
+            #  'baz.quux' = {
+            #    ...
+            #    should_be: "cool"
+            #  }
+            # }
+            #
+            # This small function is what makes this work.
+            if requirement.match?('\.')
+              root_requirement, remaining_requirements =
+                requirement.split('.')
+              remaining_requirements =
+                remaining_requirements.map { |x| "[#{x}]" }.join('')
+              # While eval is usually a security risk (arb. code execution),
+              # I don't know of another way of obtaining a hash key by string.
+              # Therefore, we'll disable this check here.
+              # rubocop:disable Security/Eval
+              requirement_under_test = eval('requirements_hash' \
+                                            "[#{root_requirement}]" \
+                                            "#{remaining_requirements}")
+              # rubocop:enable Security/Eval
+            else
+              requirement_under_test = requirements_hash[requirement]
+            end
+            test_name = requirement_under_test[:test_name]
             if test_name.downcase.match?(/^it should/)
               example_name = test_name
             else
@@ -32,10 +76,10 @@ module RSpecHelpers
                 "#{resource_name}.#{requirement}"
             end
             it example_name do
-              expected_value = requirements_hash[requirement][:should_be]
+              expected_value = requirement_under_test[:should_be]
               actual_value = @terraform_plan[resource_name][requirement.to_s]
-              matcher = if requirements_hash[requirement].key?(:matcher_type)
-                          requirements_hash[requirement][:matcher_type]
+              matcher = if requirement_under_test.key?(:matcher_type)
+                          requirement_under_test[:matcher_type]
                         else
                           :string
                         end
@@ -51,7 +95,8 @@ module RSpecHelpers
           end
         end
       end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/PerceivedComplexity
     end
   end
 end
-# rubocop:enable Metrics/AbcSize
