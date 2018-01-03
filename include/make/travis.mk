aadf576bf588be2b273a47d2d387becbfd3b96b6
Carlos Nunez <dev@carlosnunez.me>
2018-01-03 17:44:17 -0600

    [include/make/travis.mk] dev@carlosnunez.me  |  and .env should take precedence when setting vars.

--- include/make/travis.mk
+++ include/make/travis.mk
@@ -20,7 +20,7 @@ _set_travis_env_vars:
 		$(TRAVIS_CLI_DOCKER_IMAGE) -c 'gem list | grep ffi > /dev/null || apk add --update build-base libffi-dev; \
 			gem list | grep travis > /dev/null || gem install travis; \
 			travis login --github-token=$(TRAVIS_CI_GITHUB_TOKEN); \
-			(printenv | grep -E "AWS|DOCKER|TRAVIS|TERRAFORM" ; cat .env) | \
+			(cat .env ; printenv | grep -E "AWS|DOCKER|TRAVIS|TERRAFORM") | \
 			sort -u | \
 			sed -- "s/^\(.*\)=\(.*\)/travis env set -r $(TRAVIS_REPO) \1 \2 --private/" | \
 			while read command; do eval "$$command"; done'
