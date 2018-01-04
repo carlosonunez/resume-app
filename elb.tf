d36214aa20e7be0e13afec98e168d091e773527b
Carlos Nunez <dev@carlosnunez.me>
2018-01-03 20:03:15 -0600

    [elb.tf] dev@carlosnunez.me  |  They should be using IDs.

--- elb.tf
+++ elb.tf
@@ -3,5 +3,5 @@ resource "aws_lb" "lb" {
   internal = false
   load_balancer_type = "application"
   enable_deletion_protection = false
-  subnets = ["${aws_subnet.subnet_a.cidr_block}","${aws_subnet.subnet_b.cidr_block}"]
+  subnets = ["${aws_subnet.subnet_a.id}","${aws_subnet.subnet_b.id}"]
 }
