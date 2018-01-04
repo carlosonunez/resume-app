e0f6d3cef233ecf6a02f4e0074ab3c06c2f9decf
Carlos Nunez <dev@carlosnunez.me>
2018-01-03 20:29:59 -0600

    [spec/unit/terraform/service_load_balancer_spec.rb] dev@carlosnunez.me  |  Implement a target group for our load balancer

--- spec/unit/terraform/service_load_balancer_spec.rb
+++ spec/unit/terraform/service_load_balancer_spec.rb
@@ -1,23 +1,53 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-requirements = {
-  name: {
-    test_name: 'It should have the correct load balancer name',
-    should_be: 'resume-app-lb'
+resources_and_requirements = {
+  'aws_lb.lb' = {
+    name: {
+      test_name: 'It should have the correct load balancer name',
+      should_be: 'resume-app-lb'
+    },
+    internal: {
+      test_name: 'It should not be an internal load balancer',
+      should_be: 'false'
+    },
+    load_balancer_type: {
+      test_name: 'It should be an application load balancer',
+      should_be: 'application'
+    },
+    enable_deletion_protection: {
+      test_name: 'It should not have termination protection enabled',
+      should_be: 'false'
+    }
   },
-  internal: {
-    test_name: 'It should not be an internal load balancer',
-    should_be: 'false'
-  },
-  load_balancer_type: {
-    test_name: 'It should be an application load balancer',
-    should_be: 'application'
-  },
-  enable_deletion_protection: {
-    test_name: 'It should not have termination protection enabled',
-    should_be: 'false'
+  'aws_lb_target_group.target_group' = {
+    name: {
+      test_name: 'It should have the correct name',
+      should_be: 'resume-app-lb-tg'
+    },
+    port: {
+      test_name: 'It should be port 80',
+      should_be: 80
+    },
+    protocol: {
+      test_name: 'It should be using HTTP',
+      should_be: 'HTTP'
+    },
+    vpc_id: {
+      test_name: 'It should be set to the correct VPC',
+      should_be: ENV['LOAD_BALANCER_VPC']
+    },
+    'health_check.port' {
+      test_name: 'It should be set to 4567',
+      should_be: 4567
+    },
+    'health_check.protocol' {
+      test_name: 'It should be set to HTTP',
+      should_be: 'HTTP'
+    }
   }
 }
-RSpecHelpers::Terraform.run_tests(resource_name: 'aws_lb.lb',
-                                  test_definitions_hash: requirements)
+resources_and_requirements.each do |resource, requirement|
+  RSpecHelpers::Terraform.run_tests(resource_name: resource,
+                                    test_definitions_hash: requirement)
+end
