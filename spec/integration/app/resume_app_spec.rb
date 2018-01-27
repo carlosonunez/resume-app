7f980cdd341b80f4ea389ec508ac7e0d5958f29a
Carlos Nunez <dev@carlosnunez.me>
2018-01-27 17:29:25 -0600

    dev@carlosnunez.me  |  Create our integration test to verify that the application is running.

---
+++ spec/integration/app/resume_app_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Given an app that renders resumes' do
+  context 'When we access the app' do
+    it 'Should be up' do
+      uri = URI(ENV['RESUME_APP_INTEGRATION_URL'])
+      response = Net::HTTP.get(uri)
+      expect(response.status).to eq 200
+    end
+  end
+end
