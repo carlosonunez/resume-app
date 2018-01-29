3ea823e0872a11c614a990a9a600031dc1f66457
Carlos Nunez <dev@carlosnunez.me>
2018-01-28 21:52:59 -0600

    dev@carlosnunez.me  |  but we do care whether the tg gets created before or after the ECS service backing it.

--- elb.tf
+++ elb.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "lb" {
 
 resource "aws_lb_target_group" "target_group" {
   name = "resume-app-lb-tg"
-  depends_on = ["aws_lb.lb"]
+  depends_on = ["aws_ecs_service.service"]
   port = 4567
   protocol = "HTTP"
   vpc_id = "${var.load_balancer_vpc}"
@@ -20,7 +20,6 @@ resource "aws_lb_target_group" "target_group" {
 }
 
 resource "aws_lb_listener" "listener" {
-  depends_on = ["aws_lb.lb"]
   port = 80
   protocol = "HTTP"
   default_action {
