1e519490b6215dcaa7df42885fdde3b291630951
Carlos Nunez <dev@carlosnunez.me>
2018-03-21 18:25:03 -0500

    dev@carlosnunez.me  |  Separate Terraform states by environment to avoid nerfing environments accidentally.

--- include/make/terraform/s3_init.mk
+++ include/make/terraform/s3_init.mk
@@ -5,10 +5,13 @@ endif
 ifndef TERRAFORM_STATE_S3_KEY
 $(error Please define TERRAFORM_STATE_S3_KEY)
 endif
+ifndef TERRAFORM_STATE_ENVIRONMENT
+$(error Please define the environment to store your infrastructure state in.)
+endif
 
 .PHONY: _terraform_init_with_s3_backend
 _terraform_init_with_s3_backend: \
 	ADDITIONAL_TERRAFORM_ARGS = -backend-config "bucket=$(TERRAFORM_STATE_S3_BUCKET)" \
-															-backend-config "key=$(TERRAFORM_STATE_S3_KEY)" \
+															-backend-config "key=$(TERRAFORM_STATE_S3_KEY)/$(TERRAFORM_STATE_ENVIRONMENT)" \
 															-backend-config "region=$(AWS_REGION)"
 _terraform_init_with_s3_backend: _terraform_init
