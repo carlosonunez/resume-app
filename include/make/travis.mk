b0886e2a7cdc8972373f494f168984447860df87
Carlos Nunez <dev@carlosnunez.me>
2018-03-28 16:29:14 -0500

    dev@carlosnunez.me  |  Don't update env vars within Travis.

--- include/make/travis.mk
+++ include/make/travis.mk
@@ -5,6 +5,12 @@ TRAVIS_REPO=$(shell cat .git/config | \
 						sed 's;.*\:\(.*\).git;\1;' | \
 						sed 's;\/;\\\/;')
 .PHONY: _set_travis_env_vars
+ifdef TRAVIS
+set_travis_env_vars: $(info No variables to update, as we are within Travis)
+endif
+ifeq ($(DONT_UPDATE_ENVIRONMENT_VARIABLES),false)
+set_travis_env_vars: $(info Skipping environment update, as requested)
+endif
 set_travis_env_vars: verify_environment_variable_TRAVIS_CI_GITHUB_TOKEN
 set_travis_env_vars:
 	docker run --rm -t \
