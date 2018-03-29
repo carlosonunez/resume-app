a1ebfb7f60f0ec939f0f81634825477b58d72ca0
Carlos Nunez <dev@carlosnunez.me>
2018-03-29 00:16:43 -0500

    dev@carlosnunez.me  |  Tell me when this updates, as Travis is unable to see it for some reason.

--- include/make/git.mk
+++ include/make/git.mk
@@ -1,7 +1,8 @@
 #!/usr/bin/env make
 .PHONY: get_latest_commit_hash
 get_latest_commit_hash:
-	GIT_OPTIONS="HEAD" $(MAKE) _git_rev-parse | head -c 8  > version
+	GIT_OPTIONS="HEAD" $(MAKE) _git_rev-parse | head -c 8  > version; \
+	echo "$(INFO) This repository is now at version $$(cat version)"
 
 .PHONY: _git_%
 _git_%: GIT_ACTION=$(shell echo "$@" | cut -f3 -d _)
