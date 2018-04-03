fff508e724e777083f8bace402e2c9914bf98f83
Carlos Nunez <dev@carlosnunez.me>
2018-04-02 23:55:51 -0500

    dev@carlosnunez.me  |  It turns out that the environment ID wasn't being taken into accoutn when holding states.

--- include/make/terraform/s3_init.mk
+++ include/make/terraform/s3_init.mk
@@ -7,11 +7,19 @@ _terraform_init_with_s3_backend: \
 	verify_environment_variable_AWS_REGION \
 	verify_environment_variable_AWS_ACCESS_KEY_ID \
 	verify_environment_variable_AWS_SECRET_ACCESS_KEY
+ifdef ENVIRONMENT_ID
+_terraform_init_with_s3_backend: \
+	ADDITIONAL_TERRAFORM_ARGS = -backend-config "bucket=$(TERRAFORM_STATE_S3_BUCKET)" \
+															-backend-config "key=$(TERRAFORM_STATE_S3_KEY)-$(BUILD_ENVIRONMENT)-$(ENVIRONMENT_ID)" \
+															-backend-config "region=$(AWS_REGION)" \
+															-reconfigure
+else
 _terraform_init_with_s3_backend: \
 	ADDITIONAL_TERRAFORM_ARGS = -backend-config "bucket=$(TERRAFORM_STATE_S3_BUCKET)" \
 															-backend-config "key=$(TERRAFORM_STATE_S3_KEY)-$(BUILD_ENVIRONMENT)" \
 															-backend-config "region=$(AWS_REGION)" \
 															-reconfigure
+endif
 _terraform_init_with_s3_backend: _terraform_init
 
 .PHONY: _terraform_init_with_test_backend
